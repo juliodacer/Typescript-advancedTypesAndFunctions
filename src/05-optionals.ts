2cf1445cd5e9fe84cafd01827756db0f5560bc52
Julio Davila <juliodacer@gmail.com>
2023-06-11 12:10:38 -0500

    05 optionals

---
+++ src/05-optionals.ts
@@ -0,0 +1,13 @@
+export const createProduct = (
+  id: string | number,
+  isNew?: boolean,
+  stock?: number
+) => {
+  return {
+    id,
+    stock: stock ?? 10,
+    isNew: isNew ?? false
+  }
+}
+
+const p = createProduct('1')
